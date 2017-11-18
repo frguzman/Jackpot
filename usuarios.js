2d85dbe0e9c48f2e7ce28231cd5e57220e660bdd
Federico Guzmán <federico@guzman.net.ar>
2017-11-18 04:32:16 -0300

    Al ingresar un usario que no existe se lo guarda y se le asigna nombre y 50 creditos.

--- usuarios.js
+++ usuarios.js
@@ -1,39 +1,39 @@
 const usuarios = {
-    login(usuario){
-      let dato = DB[usuario];
-      
-      if(dato === undefined) {
-        $(this).trigger('fracaso');
-      } else {
-        $(this).trigger('exito', DB[usuario]);
-      }
-    },
-    registrar(usuario) {
-      if(!DB.hasOwnProperty(usuario)){
-        DB[usuario] = {
-          nombre: usuario,
-          creditos: 50
-        }
-      } else {
-        console.log('Usuario ya existe');
-      }
+  login(usuario){
+    let dato = DB[usuario];
+    
+    if(dato === undefined) {
+      $(this).trigger('fracaso', usuario);
+    } else {
+      $(this).trigger('exito', dato);
     }
-  };
-  
-  /*const usuarios = {
-    login(username){
-      const ruta = `https://istea-jackpot.firebaseio.com/usuarios/${username}.json`;
-      return $.get(ruta).done( data => $(this).trigger('exito', data) );
-    }
-  };*/
-  
-  const DB = {
-    Pepe: {
-      nombre: 'Pepe',
-      creditos: 100
-    },
-    Silvia: {
-      nombre: 'Silvia',
-      creditos: 500
+  },
+  registrar(usuario) {
+    if(!DB.hasOwnProperty(usuario)){
+      DB[usuario] = {
+        nombre: usuario,
+        creditos: 50
+      }
+    } else {
+      console.log('Usuario ya existe');
     }
   }
+};
+
+/*const usuarios = {
+  login(username){
+    const ruta = `https://istea-jackpot.firebaseio.com/usuarios/${username}.json`;
+    return $.get(ruta).done( data => $(this).trigger('exito', data) );
+  }
+};*/
+
+const DB = {
+  Pepe: {
+    nombre: 'Pepe',
+    creditos: 100
+  },
+  Silvia: {
+    nombre: 'Silvia',
+    creditos: 500
+  }
+}
